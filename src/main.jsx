4a78e5e86f9f01e3b7c27a237bfc79f8eb24a792
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-12-12 17:10:47 +0600

    authprovider setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router/Route.jsx'
+import { AuthContext } from './Authprovider/Authprovider.jsx'
 
 
 
@@ -10,6 +11,8 @@ import { router } from './Router/Route.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <AuthContext>
+      <RouterProvider router={router} />
+    </AuthContext>
   </React.StrictMode>,
 )
